5c3acea4d331555a2adc2dfca448d70b3a1fcf71
Dylan Cole <logboarddjc@gmail.com>
2021-04-12 22:06:41 -0400

    fix bug so that the function gets val from input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,18 @@
 let searchBtn = $('#search');
 // let cityName = document.querySelector('#search-box');
-let cityName = $('search-box');
+
 
 function weatherReport() {
-    // console log city name
-    console.log($('search-box').val());
+    let cityName = $('#search-box').val();
 
-//     fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=568ff9fafd7d7202b5ec934736eda812&units=imperial')
-//     .then(function(response) {
-//         if (response.ok) {
-//             return response.json();
-//         }
-//     }).then(function (something) {
-//         console.log(something);
-//     })
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=568ff9fafd7d7202b5ec934736eda812&units=imperial')
+    .then(function(response) {
+        if (response.ok) {
+            return response.json();
+        }
+    }).then(function (weatherData) {
+        
+    })
 }
 
 searchBtn.on('click', weatherReport);
